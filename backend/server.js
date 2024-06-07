bdbf5c472f69a7f46c8a1850d1bcdd75102d3616
Samarth Srivastava <90861130+Srivastava-samarth@users.noreply.github.com>
2024-06-08 01:43:17 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express' 
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
+
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+import connectToMongoDB from './db/connectTo MongoDB.js';
+import { app, server } from './socket/socket.js';
+
+
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(express.json()) // to parse the incoming requests with json payload (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
+
+// app.get("/", (req,res) => {
+//     res.send("Hello");
+// })
+
+
+server.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running at port ${PORT}`)
+});
---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+import Conversation from "./Conversation";
+import { getRandomEmoji } from "../../utils/emojis.js";
+import useGetConversations from "../../hooks/useGetConversations";
+
+const Conversations = () => {
+	const {loading,conversations} = useGetConversations();
+
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation,idx) =>(
+				<Conversation key={conversation._id}
+				conversation={conversation}
+				emoji={getRandomEmoji()}
+				lastIdx={idx === conversations.length - 1 }
+				/>
+			))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
+export default Conversations;
